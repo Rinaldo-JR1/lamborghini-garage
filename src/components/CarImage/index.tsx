f674a44886cd27d0450d06b03b14ff25c2a5853c
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-08-16 14:29:42 -0300

    Image and controls

--- src/components/CarImage/index.tsx
+++ src/components/CarImage/index.tsx
@@ -4,13 +4,17 @@ import { Image, View } from 'react-native';
 import { styles } from './styles';
 import { CAR_ASSETS_BASE_URL } from '../../constants/car';
 
-export function CarImage() {
+interface props {
+    id:number
+}
+
+export function CarImage({id}:props) {
     return (
         <View style={styles.carImageContainer}>
             <Image
                 style={styles.carImage}
                 source={{
-                    uri: `${CAR_ASSETS_BASE_URL}5.png`,
+                    uri: `${CAR_ASSETS_BASE_URL}${id}.png`,
                 }}
             />
         </View>
--- src/components/PressControls/index.tsx
+++ src/components/PressControls/index.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import React, { Dispatch, SetStateAction } from "react";
+import { Button, Text, View } from "react-native";
 
-import { styles } from './styles';
+import { styles } from "./styles";
+import { handleNextItem, handlePreviousItem } from "../../functions/cardViewActions/actions";
+import { CarModel } from "../CardView/props";
 
-export function PressControls() {
-    return (
-        <View style={styles.priceLabelContainer}>
-            <Button title='<' color={"#01A6B3"} onPress={() => { }} />
-            <Text style={styles.priceLabel}>Valor</Text>
-            <Button title=">" color={"#01A6B3"} onPress={() => { }} />
-        </View>
-    );
+interface props {
+  carData: CarModel | undefined;
+  setCarData: Dispatch<SetStateAction<CarModel | undefined>>;
+}
+
+export function PressControls({carData,setCarData}: props) {
+  return (
+    <View style={styles.priceLabelContainer}>
+      <Button
+        title="<"
+        color={"#01A6B3"}
+        onPress={() => {
+          handleNextItem(carData, setCarData);
+        }}
+      />
+      <Text style={styles.priceLabel}>Valor</Text>
+      <Button
+        title=">"
+        color={"#01A6B3"}
+        onPress={() => {
+          handlePreviousItem(carData, setCarData);
+        }}
+      />
+    </View>
+  );
 }
